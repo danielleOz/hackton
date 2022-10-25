0886e4ae18f0bd7e370480b9f85f159059daf067
danielle <danielle.oz.do@gmail.com>
2022-10-25 11:51:39 +0300

    ADD

---
+++ consts.py
@@ -0,0 +1,40 @@
+SKILLS_LIST = ["cooking", "baking", "playing music", "painting", "singing",
+               "computing", "dancing", "drawing", "sports training",
+               "hairdressing"]
+ASS_0 = {
+    "name": "heart to heart",
+    "skills": [SKILLS_LIST[5]],
+    "diss": "connecting old pepople with computers",
+    "vol_num": 5,
+    "vol_needed": 10
+}
+ASS_1 = {
+    "name": "food is the key",
+    "skills": [SKILLS_LIST[0], SKILLS_LIST[1]],
+    "diss": "cook to the people in need",
+    "vol_num": 15,
+    "vol_needed": 15
+}
+ASS_2 = {
+    "name": "graw in love",
+    "skills": [SKILLS_LIST[2], SKILLS_LIST[4], SKILLS_LIST[6]],
+    "diss": "entertain old people",
+    "vol_num": 8,
+    "vol_needed": 25
+}
+ASS_3 = {
+    "name": "coming to renovate",
+    "skills": [SKILLS_LIST[3], SKILLS_LIST[7]],
+    "diss": "renovate destroyed houses",
+    "vol_num": 7,
+    "vol_needed": 12
+}
+ASS_4 = {
+    "name": "moving",
+    "skills": [SKILLS_LIST[8]],
+    "diss": "train kids sports",
+    "vol_num": 2,
+    "vol_needed": 15
+}
+
+ASSOCIATIONS = [ASS_0, ASS_1, ASS_2, ASS_3, ASS_4]
---
+++ volenteer.py
@@ -0,0 +1,19 @@
+
+vol = None
+def create_vol(name, id , skills_list):
+    volenteer = {
+        "name":name,
+        "id": id,
+        "skills": skills_list,
+        "total hours":0
+    }
+    vol = volenteer
+
+def show_association():
+    pass
+
+def find_association():
+    pass
+
+def chosen_association():
+    pass
