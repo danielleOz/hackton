223b9b8bf9f34437f14bd7b4aa805d22f3444cc2
danielle <danielle.oz.do@gmail.com>
2022-10-25 14:44:00 +0300

    ADD

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("kkkkk")
-print("ddd")
+import consts
+import volenteer
+
+
+def show_skills():
+    for i in range(len(consts.SKILLS_LIST)):
+        print(str(i) + " : " + str(consts.SKILLS_LIST[i]))
+
+
+print("hi, welcome to volt")
+name = str(input("please type your name"))
+id = str(input("please type your id"))
+if len(id)!= 9:
+    id = str(input("wrong id, please type your id again"))
+
+skills_lst = []
+show_skills()
+skills = input("please choose the skills you have, type the index of the onees you choose devided by ',' ")
+skills_lst = skills.split(",")
+volenteer.create_vol(name,id,skills_lst)
