abffcafd09a8af2482aaf87c6e5bd56da967da6e
danielle <danielle.oz.do@gmail.com>
2022-10-26 09:58:41 +0300

    ADD

--- screen.py
+++ screen.py
@@ -6,7 +6,15 @@ app.title("volt")
 app.geometry("700x700+700+100")
 app.configure(bg = "pink")
 
+sc_chose_ass=Tk()
+sc_chose_ass.title("show_assicon")
+sc_chose_ass.geometry("700x700+700+100")
 
+def print_stars():
+ print ("**************")
+
+# lblNum = Label(app, text="Please enter a number:", height = 10)
+# lblNum.pack()
 lbl_start=Label(app, text="welcome to VOLT:",font=('Arial Bold',30), bg="pink",fg='white')
 lbl_start.grid(row=0, column=1)
 
@@ -25,19 +33,19 @@ name_enter.grid(row=1, column=1)
 id_enter=Entry(app)
 id_enter.grid(row=2, column=1)
 
-skill_enter=Entry(app)
-skill_enter.grid(row=3,column=1)
+skil_enter=Entry(app)
+skil_enter.grid(row=3,column=1)
 
 
 # decNum=Entry(app)
 # decNum.pack()
-def save_data():
+def save_date():
     name=name_enter.get()
     name_enter.delete(0, END)
     id=id_enter.get()
     id_enter.delete(0,END)
-    skill=skill_enter.get()
-    skill_enter.delete(0,END)
+    skill=skil_enter.get()
+    skil_enter.delete(0,END)
     # volenteer.create_vol(name,id,skill)
     vol=volenteer.create_vol(name,id,skill)
     print(vol)
@@ -47,7 +55,7 @@ def save_data():
     # print(name,id,skill)
 
 
-btnClick = Button(app, text = "click", width = 10,command=save_data())
+btnClick = Button(app, text = "click", width = 10,command=save_date)
 btnClick.grid(row=5, column=0)
 
 app.mainloop()
